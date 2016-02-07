7ddcfe45553b2f7d6fe9825ba9415dd94c80b58b
batouri <zeno.pacrijanot@gmail.com>
2016-02-07 23:16:45 +0100

    Init

--- src/SocialNetwork.pl
+++ src/SocialNetwork.pl
@@ -1,6 +1,6 @@
 :- module(professionalsocialnetwork, [colleague/2, manager/3, worksin/3, in_company/2, boss/2, head/1, expert/1, hasfollowers/1, comrade/2]).
 :- use_module(library(chr)).
-:- chr_constraint hasfollowers/1, worksin/3, boss/2, colleague/2, in_company/2, diff/2, adult/1, affiliated/3, manager/3, head/1, comrade/2.
+:- chr_constraint expert/1, hasfollowers/1, worksin/3, boss/2, colleague/2, in_company/2, diff/2, adult/1, affiliated/3, manager/3, head/1, comrade/2.
 
 %% FACTS
 in_company(marketing, apple).
@@ -12,7 +12,7 @@ hasfollowers(john).
 studiedat(john, economics, 2006, 2007).
 in_university(economics, descartes).
 in_university(economics, stanford).
-studiedat(bob, economics, 2006, 2007).
+studiedat(bob, econoemics, 2006, 2007).
 
 %% RULES
 manager(Name, Department, Company):-worksin(Name, Department, Company), head(Name).
